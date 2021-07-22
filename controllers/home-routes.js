d66292d31f580f7a13d502e52b12d455b0f26456
macquarts <macquart.sherine@gmail.com>
2021-07-22 14:22:28 +0800

    fixed restaurant route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -17,6 +17,26 @@ router.get('/', async (req, res) => {
   }
 });
 
+//finding a selected information and review which was created by the user and returns as a html.
+router.get('/restaurant/:id', async (req, res) => {
+  try {
+    const restaurantData = await Restaurant.findOne({
+      include: [
+        { model: Review, include: [{ model: User, attributes: ["username"] }] }
+      ],
+      where: { id: req.params.id }
+    });
+    const restaurant = restaurantData.toJSON();
+    console.log(restaurant);
+    res.render('restaurant', {
+      loggedIn: req.session.loggedIn,
+      restaurant: restaurant
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+//checks if the user is logged in if not takes user to log in page.
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
     res.redirect("/");
